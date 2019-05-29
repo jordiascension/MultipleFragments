54b69a2273cc00e958939f11f02a767d17febc4a
jordiascension <jordiascension@enterpriseconsulting.es>
2019-05-29 17:53:56 +0200

    Working With Multiple Fragments

---
+++ app/src/main/java/com/example/formexample/Main2Activity.java
@@ -0,0 +1,38 @@
+package com.example.formexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.view.View;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    public void FragmentOneClick(View view) {
+        ArticleFragment myfragment;
+        myfragment = new ArticleFragment();
+
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_switch, myfragment);
+        fragmentTransaction.commit();
+
+    }
+    public void FragmentTwoClick(View view) {
+        Fragment myfragment;
+        myfragment = new Article2Fragment();
+
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_switch, myfragment);
+        fragmentTransaction.commit();
+
+    }
+}
